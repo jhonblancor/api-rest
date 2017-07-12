e67957be814af5c756bd3597363d9290a3304e58
fill git prueba <jhonblancor@gmail.com>
2017-07-12 14:08:07 -0500

    inicio de app, api-rest: Instalacion de modulos express, body-parser, nodemon. Creacion de rutas en el index.js

--- index.js
+++ index.js
@@ -8,10 +8,34 @@ const bodyParse = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParse.urlencoded({extend(false)
-}));
+app.use(bodyParse.urlencoded({extend: false}));
+app.use(bodyParse.json());
+
+
+//creamos nuestras rutas
+app.get('/api/product',(req, res)=>{
+res.send(200,{priducts:[]});
+});
+
+app.get('/api/product/:productId',(req,res)=>{
+
+});
+
+app.post('/api/product',(req,res)=>{
+console.log(req.body);
+res.status(200).send(200,{message:'el producto se ha recibido'})
+});
+
+app.put('api/product/:productId',(req,res)=>{
+
+});
+
+app.delete('/api/product/:productId',(req,res)=>{
+
+});
+
 //le decimnos que escuche por el puerto 3000 y con la nueva
 //arrofunctio de ecmascript 6 que imprima Api rest
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
 	console.log('API REST corriendo')
 });
